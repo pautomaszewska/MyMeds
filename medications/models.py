eca22f12f83219ae2f72f3dab42cf8e7c5614103
pautomaszewska <ptomaszewska@gmail.com>
2019-06-03 20:38:45 +0200

    Added settings.py example file

--- medications/models.py
+++ medications/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class Medicine(models.Model):
     name = models.CharField(max_length=64)
-    active_ingredient = models.CharField(max_length=128, null=True)
+    active_ingredient = models.CharField(max_length=128, null=True, blank=True)
     amount = models.IntegerField(null=True)
-    dose = models.CharField(max_length=64, null=True)
+    dose = models.CharField(max_length=64, null=True, blank=True)
     expiration_date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
